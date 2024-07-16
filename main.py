20a2a11befb08ad30623173dfaa21ca287f87437
faby440 <faby440@gmail.com>
2024-07-16 13:52:53 -0400

    tercer cambio

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 import data
 from methods import UrbanRoutesPage
-
 class TestUrbanRoutes:
+
     driver = None
+
     @classmethod
     def setup_class(cls):
         chrome_options = webdriver.ChromeOptions()
@@ -12,15 +14,16 @@ class TestUrbanRoutes:
         cls.driver = webdriver.Chrome(options=chrome_options)
         cls.driver.get(data.urban_routes_url)
         cls.routes_page = UrbanRoutesPage(cls.driver)
-    def test_set_routes(self):
+    def test_set_route(self):
+        self.driver.get(data.urban_routes_url)
         routes_page = UrbanRoutesPage(self.driver)
         address_from = data.address_from
         address_to = data.address_to
-        routes_page.set_from(self, address_from)
-        routes_page.set_to(self, address_to)
-        # assert routes_page.get_from() == data.address_from
-        #assert routes_page.get_to() == data.address_to
+        routes_page.set_route(address_from, address_to)
+        assert routes_page.get_from() == address_from
+        assert routes_page.get_to() == address_to
 
+        WebDriverWait(Driver, 6)
     @classmethod
     def teardown_class(cls):
         cls.driver.quit()
--- methods.py
+++ methods.py
@@ -3,26 +3,24 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-import json
 import time
 import data
 from selenium.common import WebDriverException
 
 class UrbanRoutesPage:
-
     def __init__(self, driver):
         self.driver = driver
 
 #ingresa direction desde
-    def set_from(self, from_address):
-        self.driver.find_element(*self.from_field).send_keys(from_address)
+    def set_from(self, address_from):
+        self.driver.find_element(*self.from_field).send_keys(data.address_from)
 
 #ingresa direction hasta
-    def set_to(self, to_address):
-        self.driver.find_element(*self.to_field).send_keys(to_address)
+    def set_to(self, address_to):
+        self.driver.find_element(*self.to_field).send_keys(data.address_to)
 
     def get_from(self):
-        return self.driver.find_element(self.from_field).get_property('value')
+        return self.driver.find_element(*self.from_field).get_property('value')
 
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
@@ -32,61 +30,71 @@ class UrbanRoutesPage:
         self.driver.find_element(*self.button_round).click()
 
 # click seleccion taxi confort
-    def click_tcard_active(self):
-        self.driver.find_element(*self.tcard_active).click()
+    def click_confort_tcard_active(self):
+        self.driver.find_element(*self.confort_tcard_active).click()
+
+    def click_button_phone(self):
+        self.driver.find_element(*self.button_phone).click()
 
 # Ingresar numero de tlf
-    def set_number(self, number_phone):
-        self.driver.find_element(*self.number_field).send_keys(data.number_phone)
+    def set_number(self, phone_number):
+        self.driver.find_element(*self.number_field).send_keys(data.phone_number)
 
+    def click_selection_button_next(self):
+        self.driver.find_element(*self.selection_button_next).click()
+
+    def click_code(self):
+        self.driver.find_element(*self.code_field).click()
+
+    def retrieve_phone_code(driver) -> str:
+        """Este código devuelve un número de confirmación de teléfono y lo devuelve como un string.
+        Utilízalo cuando la aplicación espere el código de confirmación para pasarlo a tus pruebas.
+        El código de confirmación del teléfono solo se puede obtener después de haberlo solicitado en la aplicación."""
     def get_number(self):
         return self.driver.find_element(self.number_field).get_property('value')
 
 # click en metodo de pago
-    def click_button_filled(self):
-        self.driver.find_element(*self.button_filled).click()
+    def click_methods_pay(self):
+        self.driver.find_element(*self.methods_pay).click()
 
 # click agregar tarjeta
-    def click_title(self):
-        self.driver.find_element(*self.locators.title).click()
+    def click_to_add_card(self):
+        self.driver.find_element(*self.to_add_card).click()
 
-# ingresa numero de tarjeta
+#ingresa numero de tarjeta
     def set_card_number(self, card_number):
-        self.driver.find_element(*self.number_field).send_keys(data.card_number)
-
+        self.driver.find_element(*self.card_number_field).send_keys(data.card_number)
 #ingresa numero de codigo
-    def set_code(self, card_code):
+    def set_card_code(self, card_code):
         self.driver.find_element(*self.code_field).send_keys(data.card_code)
+
+    def click_botton_to_add(self):
+        self.driver.find_element(*self.botton_to_add).click()
+
+    def click_close_button(self):
+        self.driver.find_element(*self.close_button).click()
 
     def get_card_number(self):
         return self.driver.find_element(self.number_field).get_property('value')
 
-    def get_code(self):
+    def get_card_code(self):
         return self.driver.find_element(*self.code_field).get_property('value')
 
-#clik en el boton agregar
-    def click_button_full(self):
-        self.driver.find_element(*self.button_full).click()
-
-# mensaje al conductor
+#mensaje al conductor
     def set_message_for_driver(self, message_for_driver):
         self.driver.find_element(*self.message_for_driver_field).send_keys(data.message_for_driver)
 
-# click para escoger manta y pañuelo
-    def slider_round(self):
-       self.driver.find_element(*self.locators.slider_round).click()
+#click para escoger manta y pañuelo
+    def selecction_blanket(self):
+       self.driver.find_element(*self.selecction_blanket).click()
 
  # pedir dos helados
-    def counter_plus(self):
-        self.driver.find_element(*self.locator.counter_plus).click()
+    def selecction_ice(self):
+        self.driver.find_element(*self.selecction_ice).click()
 
-    def counter_plus(self):
-        self.driver.find_element(*self.locators.counter_plus).click()
+    def selecction_ice(self):
+        self.driver.find_element(*self.selecction_ice).click()
 
- # click aceptar taxi
+#click aceptar taxi
     def smart_button_main(self):
-        self.driver.find_element(*self.locators.smart_button_main).click()
-
-    def set_routes(self, address_from, address_to):
-        self.set_from(address_from)
-        self.set_to(address_to)
+        self.driver.find_element(*self.smart_button_main).click()
